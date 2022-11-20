abafd8aa574671524a4eb8ae34e40ec881668b81
dot-tod <vadya1987@gmail.com>
2022-11-20 16:53:06 +0700

    bin/brain-games and src/cli.js have been modified by eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
+const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
   return userName;
 };
+
+export default getName;
