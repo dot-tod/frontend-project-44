aa94c6db7fda24cb17529df4720f4a8f05e0e263
dot-tod <vadya1987@gmail.com>
2022-11-21 19:53:08 +0700

    modified bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
@@ -11,25 +10,17 @@ while (count < 3) {
   const randomInt = Math.floor(Math.random() * 100);
   console.log(`Question: ${randomInt}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  if (userAnswer === 'yes') {
-    if (randomInt % 2 === 0) {
-      console.log('Correct!');
-      count += 1;
-    } else {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    }
+  if ((userAnswer === 'yes' && randomInt % 2 === 0)
+    || (userAnswer === 'no' && randomInt % 2 !== 0)) {
+    console.log('Correct!');
+    count += 1;
+    continue;
+  } else if (userAnswer === 'yes') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+  } else if (userAnswer === 'no') {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
   }
-  if (userAnswer === 'no') {
-    if (randomInt % 2 !== 0) {
-      console.log('Correct!');
-      count += 1;
-    } else {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    }
-  }
+  console.log(`Let's try again, ${userName}!`);
+  break;
 }
 if (count === 3) console.log(`Congratulations, ${userName}!`);
